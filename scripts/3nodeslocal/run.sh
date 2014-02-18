d79ffa4708f5271f6bcb0c55bcb16f969b297e7d
Westy <westy@cs.umass.edu>
2014-02-18 02:58:15 +0000

    happy to be rid of python config files :-)
    git-svn-id: svn+ssh://none.cs.umass.edu/svn/mobility-first/GNS/trunk@393 0b15c3b6-4604-4ca4-ad59-2e1af984e93c

--- scripts/3nodeslocal/run.sh
+++ scripts/3nodeslocal/run.sh
@@ -1,5 +1,11 @@
 #
-./name-server.py --id 0 --nsFile name-server-info  --dataStore MONGO --location --consoleOutputLevel INFO --fileLoggingLevel FINE &
-./name-server.py --id 1 --nsFile name-server-info  --dataStore MONGO --location --consoleOutputLevel INFO --fileLoggingLevel FINE &
-./name-server.py --id 2 --nsFile name-server-info  --dataStore MONGO --location --consoleOutputLevel INFO --fileLoggingLevel FINE &
-./local-name-server.py --id 3 --nsFile name-server-info --runHttpServer --consoleOutputLevel INFO --fileLoggingLevel FINE &
+java -Xmx2g -cp ../../build/jars/GNS.jar edu.umass.cs.gns.main.StartNameServer -id 0 -configFile ns.conf &
+java -Xmx2g -cp ../../build/jars/GNS.jar edu.umass.cs.gns.main.StartNameServer -id 1 -configFile ns.conf &
+java -Xmx2g -cp ../../build/jars/GNS.jar edu.umass.cs.gns.main.StartNameServer -id 2 -configFile ns.conf &
+java -Xmx2g -cp ../../build/jars/GNS.jar edu.umass.cs.gns.main.StartLocalNameServer -id 3 -configFile lns.conf &
+
+
+#./name-server.py --id 0 --nsFile name-server-info  --dataStore MONGO --location --consoleOutputLevel INFO --fileLoggingLevel FINE &
+#./name-server.py --id 1 --nsFile name-server-info  --dataStore MONGO --location --consoleOutputLevel INFO --fileLoggingLevel FINE &
+#./name-server.py --id 2 --nsFile name-server-info  --dataStore MONGO --location --consoleOutputLevel INFO --fileLoggingLevel FINE &
+#./local-name-server.py --id 3 --nsFile name-server-info --runHttpServer --consoleOutputLevel INFO --fileLoggingLevel FINE &
