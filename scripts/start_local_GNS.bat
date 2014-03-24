7f91eb932d08057ed26f82d9a8a1638414c6d51f
Abhigyan Sharma <abhigyan@cs.umass.edu>
2014-03-24 23:55:37 +0000

    MOB-150 updated Emmanuel's batch file with updated config file  location
    git-svn-id: svn+ssh://none.cs.umass.edu/svn/mobility-first/GNS/trunk@520 0b15c3b6-4604-4ca4-ad59-2e1af984e93c

--- scripts/start_local_GNS.bat
+++ scripts/start_local_GNS.bat
@@ -1,4 +1,4 @@
 @echo off
 start "MongoDB" C:\mongodb\bin\mongod.exe --dbpath c:\mongodb\data
-start "Name Server" java -cp ..\dist\GNS.jar edu.umass.cs.gns.nsdesign.StartNameServer -id 0 -nsfile ..\resources\testCodeResources\singleNStest\node_config_1ns_1lns -primary 1
-start "Local Name Server" java -cp ..\dist\GNS.jar edu.umass.cs.gns.main.StartLocalNameServer -id 1 -nsfile ..\resources\testCodeResources\singleNStest\node_config_1ns_1lns -primary 1
+start "Name Server" java -cp ..\dist\GNS.jar edu.umass.cs.gns.nsdesign.StartNameServer -id 0 -nsfile ..\conf\singleNStest\node_config_1ns_1lns -primary 1
+start "Local Name Server" java -cp ..\dist\GNS.jar edu.umass.cs.gns.main.StartLocalNameServer -id 1 -nsfile ..\conf\singleNStest\node_config_1ns_1lns -primary 1
