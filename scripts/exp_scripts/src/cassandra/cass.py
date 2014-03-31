0d73ceecd8653c1073f680291805ade6630d0317
Rahul <rahulram@cs.umass.edu>
2014-03-31 23:14:14 +0000

    added logging and execptional handling
    git-svn-id: svn+ssh://none.cs.umass.edu/svn/mobility-first/GNS/trunk@561 0b15c3b6-4604-4ca4-ad59-2e1af984e93c

--- scripts/exp_scripts/src/cassandra/cass.py
+++ scripts/exp_scripts/src/cassandra/cass.py
@@ -1,20 +1,31 @@
-import os 
+import os,sys 
 import pycassa 
+import logging 
 from pycassa.pool import ConnectionPool
 from pycassa.columnfamily import ColumnFamily
+from datetime import datetime as d 
 
 __author__ = 'rahul'
 
 
-pool = ConnectionPool('keyspace_name')
-col_fam = ColumnFamily(pool, 'table_name')
+logging.basicConfig(filename="example.log",level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S')
+
+pool = ConnectionPool('icepice')
+col_fam = ColumnFamily(pool, 'info')
 
 #Sample Insert operations 
-col_fam.insert('row_key',{'col_name' : 'value' })
+#col_fam.insert('row_key',{'col_name' : 'value' })
 
 '''Sample Get operations . 
 Need to wrap the get operations in exception block for safe failing from
 'key not found error' .
 '''
-col_fam.get('row_key')
+try:
+  for i in range(0,1000):
+    s = d.now()
+    col_fam.get(i)
+    logging.info(str(i) +"=>"+ str((d.now() - s).microseconds))
+
+except:
+  logging.info("Error" + str(sys.exc_info()[0]))
 
