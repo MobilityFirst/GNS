92f58aaee97c554cd3946192a62246e853744046
Abhigyan Sharma <abhigyan@cs.umass.edu>
2014-04-08 20:59:00 +0000

    forgot to add to svn
    git-svn-id: svn+ssh://none.cs.umass.edu/svn/mobility-first/GNS/trunk@607 0b15c3b6-4604-4ca4-ad59-2e1af984e93c

---
+++ src/edu/umass/cs/gns/nsdesign/activeReconfiguration/ActiveReplicaApp.java
@@ -0,0 +1,97 @@
+package edu.umass.cs.gns.nsdesign.activeReconfiguration;
+
+
+import edu.umass.cs.gns.main.GNS;
+import edu.umass.cs.gns.nsdesign.Config;
+import edu.umass.cs.gns.nsdesign.Reconfigurable;
+import edu.umass.cs.gns.nsdesign.Replicable;
+import edu.umass.cs.gns.nsdesign.packet.OldActiveSetStopPacket;
+import edu.umass.cs.gns.nsdesign.packet.Packet;
+import edu.umass.cs.gns.nsdesign.replicaController.Application;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+@author V. Arun
+ */
+public class ActiveReplicaApp implements Reconfigurable, Replicable {
+
+	Application app=null;
+	ActiveReplica activeReplica = null;
+
+	public ActiveReplicaApp(Application app, ActiveReplica activeReplica) {
+		this.app = app;
+    this.activeReplica = activeReplica;
+	}
+
+  private void assertReconfigurable() {
+		if(!(this.app instanceof Reconfigurable)) 
+			throw new RuntimeException("Attempting to reconfigure an application that is not reconfigurable");
+	}
+
+
+	@Override
+	public boolean handleDecision(String name, String value, boolean recovery) {
+    boolean executed = false;
+    try {
+      JSONObject json = new JSONObject(value);
+      if (Packet.getPacketType(json).equals(Packet.PacketType.OLD_ACTIVE_STOP)) {
+        boolean noCoordinationState = json.has(Config.NO_COORDINATOR_STATE_MARKER);
+        if (noCoordinationState) {
+          executed = false;
+          // ignore
+          GNS.getLogger().severe("No coordinator state found for stop request: " + value);
+        }
+       else {
+          executed = stopVersion(name, (short) -1);
+          if (executed) {
+            try {
+              activeReplica.stopProcessed(new OldActiveSetStopPacket(new JSONObject(value)));
+            } catch (JSONException e) {
+              e.printStackTrace();
+            }
+          }
+        }
+      }
+      else executed = this.app.handleDecision(name, value, recovery);
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+
+		return executed;
+	}
+
+	@Override
+	public boolean stopVersion(String name, short version) {
+		assertReconfigurable();
+		return ((Reconfigurable)(this.app)).stopVersion(name, version);
+	}
+
+	@Override
+	public String getFinalState(String name, short version) {
+		assertReconfigurable();
+		return ((Reconfigurable)this.app).getFinalState(name, version);
+	}
+
+	@Override
+	public void putInitialState(String name, short version, String state) {
+		assertReconfigurable();
+		((Reconfigurable)this.app).putInitialState(name, version, state);
+	}
+
+	@Override
+	public int deleteFinalState(String name, short version) {
+		assertReconfigurable();
+		return ((Reconfigurable)(this.app)).deleteFinalState(name, version);
+	}
+
+  @Override
+  public String getState(String name) {
+    return ((Replicable)app).getState(name);
+  }
+
+  @Override
+  public boolean updateState(String name, String state) {
+    return ((Replicable)app).updateState(name, state);
+  }
+}
