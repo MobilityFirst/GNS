3ec75db61be0748f7b7aa95a23446d9ec7dfd200
Abhigyan Sharma <abhigyan@cs.umass.edu>
2014-05-26 19:16:54 +0000

    [Experiment-related]
    git-svn-id: svn+ssh://none.cs.umass.edu/svn/mobility-first/GNS/trunk@783 0b15c3b6-4604-4ca4-ad59-2e1af984e93c

--- src/edu/umass/cs/gns/test/StartExperiment.java
+++ src/edu/umass/cs/gns/test/StartExperiment.java
@@ -1,6 +1,8 @@
 package edu.umass.cs.gns.test;
 
 import edu.umass.cs.gns.localnameserver.ClientRequestHandlerInterface;
+import edu.umass.cs.gns.main.GNS;
+import edu.umass.cs.gns.test.connecttime.StartConnectTimeExperiment;
 import edu.umass.cs.gns.test.testclient.LNSSideImplementation;
 
 import java.io.IOException;
@@ -15,22 +17,23 @@ public class StartExperiment {
   public void startMyTest(int nodeID, String workloadFile, String updateTraceFile,
                           ClientRequestHandlerInterface handler)
           throws IOException, InterruptedException {
-    LNSSideImplementation.startServer();
-//    GNS.getLogger().info("Workload config file: " + workloadFile);
-//    WorkloadParams params = new WorkloadParams(workloadFile);
-//
-//    if (updateTraceFile != null || params.getExpType().equals(ExpType.TRACE)) {
-//      GNS.getStatLogger().info("Starting trace based experiment ... ");
-//      new NewRequestGenerator(new WorkloadParams(workloadFile), updateTraceFile, handler);
-//    }
-//    else if (params.getExpType().equals(ExpType.CONNECT_TIME)) {
-//      GNS.getStatLogger().info("Starting connect time experiment ... ");
-//      StartConnectTimeExperiment.startTest(nodeID, params);
-//    }
-//    else if (params.getExpType().equals(ExpType.BASICTEST)) {
-//      GNS.getStatLogger().info("Starting basic test ... ");
+
+    GNS.getLogger().info("Workload config file: " + workloadFile);
+    WorkloadParams params = new WorkloadParams(workloadFile);
+
+    if (updateTraceFile != null || params.getExpType().equals(ExpType.TRACE)) {
+      GNS.getStatLogger().info("Starting trace based experiment ... ");
+      new NewRequestGenerator(new WorkloadParams(workloadFile), updateTraceFile, handler);
+    }
+    else if (params.getExpType().equals(ExpType.CONNECT_TIME)) {
+      GNS.getStatLogger().info("Starting connect time experiment ... ");
+      StartConnectTimeExperiment.startTest(nodeID, params);
+    }
+    else if (params.getExpType().equals(ExpType.BASICTEST)) {
+      GNS.getStatLogger().info("Starting basic test ... ");
+      LNSSideImplementation.startServer();
 //      Test1Name.startTest();
-//    }
+    }
 
   }
 }
