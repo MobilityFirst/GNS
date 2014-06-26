49aa8430460d2f706e7a0bfbe3e98d9e0d1bb213
Arun Venkataramani <arun@cs.umass.edu>
2014-06-26 20:49:17 +0000

    git-svn-id: svn+ssh://none.cs.umass.edu/svn/mobility-first/GNS/trunk@876 0b15c3b6-4604-4ca4-ad59-2e1af984e93c

--- src/edu/umass/cs/gns/nio/BasicPacketDemultiplexer.java
+++ src/edu/umass/cs/gns/nio/BasicPacketDemultiplexer.java
@@ -7,20 +7,20 @@ import java.util.concurrent.TimeUnit;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import edu.umass.cs.gns.main.GNS;
 import edu.umass.cs.gns.nsdesign.packet.Packet;
 
 public abstract class BasicPacketDemultiplexer {
 	
 	/********************************** Start of new, untested parts ***************************/
-	ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(10); // FIXME: Not sure on what basis to limit number of threads
-	HashMap<Packet.PacketType, Boolean> demuxMap = new HashMap<Packet.PacketType, Boolean>();
+	private ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(10); // FIXME: Not sure on what basis to limit number of threads
+	private HashMap<Packet.PacketType, Boolean> demuxMap = new HashMap<Packet.PacketType, Boolean>();
 
 	public boolean register(Packet.PacketType type) {
 		boolean isFirst = this.demuxMap.containsKey(type);
 		this.demuxMap.put(type, true);
 		return isFirst;
 	}
+	// This method will be invoked by NIO 
 	public boolean handleJSONObjectSuper(JSONObject jsonObject) throws JSONException {
 		Tasker tasker = new Tasker(jsonObject);
 		boolean handled = this.demuxMap.containsKey(Packet.getPacketType(jsonObject));
@@ -34,12 +34,7 @@ public abstract class BasicPacketDemultiplexer {
 			this.json = json;
 		}
 		public void run() {
-			try {
-				handleJSONObjectSuper(this.json);
-			} catch(JSONException je) {
-				GNS.getLogger().severe("Can not parse packet type in " + this.json);
-				je.printStackTrace();
-			}
+			handleJSONObject(this.json);
 		}
 	}
 	/********************************** End of new, untested parts ***************************/
