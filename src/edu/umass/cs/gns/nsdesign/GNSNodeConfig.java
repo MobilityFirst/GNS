4ca7f8b95b7d4832aae0b507f736af1eeaa6c952
Westy <westy@cs.umass.edu>
2014-07-02 20:18:42 +0000

    more work on MOB-256; fixed issue with getting local host
    git-svn-id: svn+ssh://none.cs.umass.edu/svn/mobility-first/GNS/trunk@901 0b15c3b6-4604-4ca4-ad59-2e1af984e93c

--- src/edu/umass/cs/gns/nsdesign/GNSNodeConfig.java
+++ src/edu/umass/cs/gns/nsdesign/GNSNodeConfig.java
@@ -569,48 +569,50 @@ public class GNSNodeConfig implements InterfaceNodeConfig {
    * @throws UnknownHostException If the LAN address of the machine cannot be found.
    */
   public static InetAddress getLocalHostLANAddress() throws UnknownHostException {
-    try {
-      InetAddress candidateAddress = null;
-      // Iterate all NICs (network interface cards)...
-      for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements();) {
-        NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
-        // Iterate all IP addresses assigned to each card...
-        for (Enumeration inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements();) {
-          InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
-          if (!inetAddr.isLoopbackAddress()) {
-
-            if (inetAddr.isSiteLocalAddress()) {
-              // Found non-loopback site-local address. Return it immediately...
-              return inetAddr;
-            } else if (candidateAddress == null) {
-              // Found non-loopback address, but not necessarily site-local.
-              // Store it as a candidate to be returned if site-local address is not subsequently found...
-              candidateAddress = inetAddr;
-              // Note that we don't repeatedly assign non-loopback non-site-local addresses as candidates,
-              // only the first. For subsequent iterations, candidate will be non-null.
-            }
-          }
-        }
-      }
-      if (candidateAddress != null) {
-        // We did not find a site-local address, but we found some other non-loopback address.
-        // Server might have a non-site-local address assigned to its NIC (or it might be running
-        // IPv6 which deprecates the "site-local" concept).
-        // Return this non-loopback candidate address...
-        return candidateAddress;
-      }
-      // At this point, we did not find a non-loopback address.
-      // Fall back to returning whatever InetAddress.getLocalHost() returns...
-      InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
-      if (jdkSuppliedAddress == null) {
-        throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
-      }
-      return jdkSuppliedAddress;
-    } catch (Exception e) {
-      UnknownHostException unknownHostException = new UnknownHostException("Failed to determine LAN address: " + e);
-      unknownHostException.initCause(e);
-      throw unknownHostException;
-    }
+    return InetAddress.getLocalHost();
+    // BELOW THIS IS FAILING FOR CERTAIN SITUATIONS
+//    try {
+//      InetAddress candidateAddress = null;
+//      // Iterate all NICs (network interface cards)...
+//      for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements();) {
+//        NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
+//        // Iterate all IP addresses assigned to each card...
+//        for (Enumeration inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements();) {
+//          InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
+//          if (!inetAddr.isLoopbackAddress()) {
+//
+//            if (inetAddr.isSiteLocalAddress()) {
+//              // Found non-loopback site-local address. Return it immediately...
+//              return inetAddr;
+//            } else if (candidateAddress == null) {
+//              // Found non-loopback address, but not necessarily site-local.
+//              // Store it as a candidate to be returned if site-local address is not subsequently found...
+//              candidateAddress = inetAddr;
+//              // Note that we don't repeatedly assign non-loopback non-site-local addresses as candidates,
+//              // only the first. For subsequent iterations, candidate will be non-null.
+//            }
+//          }
+//        }
+//      }
+//      if (candidateAddress != null) {
+//        // We did not find a site-local address, but we found some other non-loopback address.
+//        // Server might have a non-site-local address assigned to its NIC (or it might be running
+//        // IPv6 which deprecates the "site-local" concept).
+//        // Return this non-loopback candidate address...
+//        return candidateAddress;
+//      }
+//      // At this point, we did not find a non-loopback address.
+//      // Fall back to returning whatever InetAddress.getLocalHost() returns...
+//      InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
+//      if (jdkSuppliedAddress == null) {
+//        throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
+//      }
+//      return jdkSuppliedAddress;
+//    } catch (Exception e) {
+//      UnknownHostException unknownHostException = new UnknownHostException("Failed to determine LAN address: " + e);
+//      unknownHostException.initCause(e);
+//      throw unknownHostException;
+//    }
   }
 
   /**
