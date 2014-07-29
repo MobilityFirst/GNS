b0af2f683f5da2bf50b6d8f2efe7cf52b811eef7
Arun Venkataramani <arun@cs.umass.edu>
2014-07-29 19:42:00 +0000

    All deprecated classes anyway. Just adding them to fix compilation.
    git-svn-id: svn+ssh://none.cs.umass.edu/svn/mobility-first/GNS/trunk@961 0b15c3b6-4604-4ca4-ad59-2e1af984e93c

---
+++ src/edu/umass/cs/gns/activereplica/scratch/NewActiveStartInfo.java
@@ -0,0 +1,38 @@
+package edu.umass.cs.gns.activereplica.scratch;
+
+import edu.umass.cs.gns.nsdesign.packet.NewActiveSetStartupPacket;
+import edu.umass.cs.gns.replicaCoordination.multipaxos.paxosutil.WaitforUtility;
+import edu.umass.cs.gns.util.Util;
+
+/**
+ * @author V. Arun
+ * Based on code created by abhigyan on 3/28/14.
+ * 
+ * FIXME: Arun: Unclear what the point of this class is. Needs to be
+ * documented. It seems to be waiting for a majority of responses 
+ * from (old or new?) active replicas. Should use WaitforUtility in
+ * multipaxos.
+ */
+public class NewActiveStartInfo {
+
+	public final NewActiveSetStartupPacket originalPacket;
+	private final WaitforUtility waitfor;
+	boolean sent = false;
+
+	public NewActiveStartInfo(NewActiveSetStartupPacket originalPacket) {
+		this.originalPacket = originalPacket;
+		this.waitfor = new WaitforUtility(Util.setToIntArray(originalPacket.getNewActiveNameServers()));
+	}
+
+	public synchronized boolean receivedResponseFromActive(int ID) {
+		return this.waitfor.updateHeardFrom(ID);
+	}
+
+	public synchronized boolean haveMajorityActivesResponded() {
+		if (sent == false && waitfor.heardFromMajority()) {
+			sent = true;
+			return true;
+		}
+		return false;
+	}
+}
