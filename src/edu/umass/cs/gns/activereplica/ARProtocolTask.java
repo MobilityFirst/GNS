daab3cb5cdd6cd401a68addc06fe0f8ab6fa97af
Westy <westy@cs.umass.edu>
2014-09-26 19:31:38 +0000

    Removed activereplica package.
    git-svn-id: svn+ssh://none.cs.umass.edu/svn/mobility-first/GNS/branches/v1.11-2014-9-22-arun-merge@1134 0b15c3b6-4604-4ca4-ad59-2e1af984e93c

--- src/edu/umass/cs/gns/activereplica/ARProtocolTask.java
+++
@@ -1,7 +0,0 @@
-package edu.umass.cs.gns.activereplica;
-/**
-@author V. Arun
- */
-public interface ARProtocolTask extends Runnable{
-	public void setActiveReplica(ActiveReplica<?> activeReplica);
-}
