a3d21d2d1309f2aba33f7ed0f74ad2151f03e053
Arun Venkataramani <arun@cs.umass.edu>
2014-12-22 18:02:18 +0000

    git-svn-id: svn+ssh://none.cs.umass.edu/svn/mobility-first/GNS/trunk@1323 0b15c3b6-4604-4ca4-ad59-2e1af984e93c

---
+++ src/edu/umass/cs/gns/gigapaxos/paxosutil/ConsumerBatchTask.java
@@ -0,0 +1,59 @@
+package edu.umass.cs.gns.gigapaxos.paxosutil;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+
+public abstract class ConsumerBatchTask<TaskType> extends
+		ConsumerTask<TaskType> {
+
+	private final TaskType[] dummy;
+
+	public ConsumerBatchTask(Collection<TaskType> lock, TaskType[] dummy) {
+		super(lock);
+		this.dummy = Arrays.copyOf(dummy, 0);
+	}
+	public ConsumerBatchTask(Map<?,TaskType> lock, TaskType[] dummy) {
+		super(lock);
+		this.dummy = Arrays.copyOf(dummy, 0);
+	}
+
+	public abstract void enqueueImpl(TaskType task);
+
+	public abstract TaskType dequeueImpl();
+
+	public abstract void process(TaskType task);
+
+	public abstract void process(TaskType[] tasks);
+
+	@SuppressWarnings("unchecked")
+	protected TaskType[] dequeueAll() {
+		synchronized (lock) {
+			TaskType[] tasks = null;
+			if(this.lock instanceof Collection) {
+				tasks = ((Collection<?>)this.lock).toArray(dummy);
+				((Collection<?>)this.lock).clear();
+				return tasks;
+			}
+			else if(this.lock instanceof Map) {
+				tasks = ((Map<?,TaskType>)this.lock).values().toArray(dummy);
+				((Map<?,TaskType>)this.lock).clear();
+				return tasks;
+				
+			}
+			throw new RuntimeException("ConsumerBatchTask lock must be of type Collection<TaskType");
+		}
+	}
+
+	public void run() {
+		while (true) {
+			waitForNotEmptyOrStopped();
+			if (isStopped())
+				break;
+
+			this.setProcessing(true);
+			process(dequeueAll());
+			this.setProcessing(false);
+		}
+	}
+}
