68d764c90fdf490e76ce3096bb3320e56b8c8520
Westy <westy@cs.umass.edu>
2015-04-10 12:27:26 -0400

    Fix for MOB-403: java.lang.AssertionError: 355 != 319 followed by what appears to be a garbled serviceName The code there expected headeredMsg.length() to equal headeredMsg.getBytes(). But the former is the length assuming Unicode while the latter is with the default encoding.

--- src/edu/umass/cs/gns/nio/JSONNIOTransport.java
+++ src/edu/umass/cs/gns/nio/JSONNIOTransport.java
@@ -123,9 +123,12 @@ public class JSONNIOTransport<NodeIDType> extends NIOTransport<NodeIDType> imple
     int written = 0;
     int originalSize = jsonData.toString().length();
     //stampSenderInfo(jsonData);
-    String headeredMsg = JSONMessageExtractor.prependHeader(jsonData.toString());
-    written = this.sendUnderlying(isa, headeredMsg.getBytes())
-            - (headeredMsg.length() - originalSize); // subtract header length
+    byte[] headeredMsg = JSONMessageExtractor.prependHeader(jsonData.toString()).getBytes();
+    //String headeredMsg = JSONMessageExtractor.prependHeader(jsonData.toString());
+    written = this.sendUnderlying(isa, headeredMsg) - 
+                          (headeredMsg.length - originalSize); // subtract header length
+//    written = this.sendUnderlying(isa, headeredMsg.getBytes())
+//            - (headeredMsg.length() - originalSize); // subtract header length
     assert (written < 0 || written == originalSize);
     return written;
   }
@@ -143,9 +146,12 @@ public class JSONNIOTransport<NodeIDType> extends NIOTransport<NodeIDType> imple
 		  sendLocal(jsonData); 
 		  written = originalSize; // local send just passes pointers
 	  } else {
-		  String headeredMsg = JSONMessageExtractor.prependHeader(jsonData.toString());
-		  written = (this.sendUnderlying(destID, headeredMsg.getBytes())
-				  - (headeredMsg.length() - originalSize)); // subtract header length
+                  byte[] headeredMsg = JSONMessageExtractor.prependHeader(jsonData.toString()).getBytes();
+		  //String headeredMsg = JSONMessageExtractor.prependHeader(jsonData.toString());
+                  written = this.sendUnderlying(destID, headeredMsg) - 
+                          (headeredMsg.length - originalSize); // subtract header length
+//		  written = (this.sendUnderlying(destID, headeredMsg.getBytes())
+//				  - (headeredMsg.length() - originalSize)); // subtract header length
 	  }
 	  assert(written < 0 || written == originalSize) : written + " != " + originalSize;
 	  return written;
