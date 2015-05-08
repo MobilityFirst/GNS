703781c739925eaaeed2587314f2e0b95b2b22dd
V. Arun <arun@cs.umass.edu>
2015-05-08 13:47:25 -0400

    Overhaul to reconfiguration package to support node adds and deletes. Detailed documentation coming in future commit.

---
+++ src/edu/umass/cs/gns/reconfiguration/reconfigurationutils/StringLocker.java
@@ -0,0 +1,14 @@
+package edu.umass.cs.gns.reconfiguration.reconfigurationutils;
+
+import java.util.HashMap;
+
+public class StringLocker {
+	private final HashMap<String,String> map = new HashMap<String,String>();
+	
+	public String get(String key) {
+		if(map.containsKey(key)) return map.get(key);
+		String newKey = new String(key);
+		map.put(newKey, newKey);
+		return newKey;
+	}
+}
