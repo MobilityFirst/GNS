e413b71c6337f7b0d308739fef55b8f2a8477a9b
Westy <westy@cs.umass.edu>
2015-06-22 23:02:23 -0400

    Updated build.xml to generate nio jar

--- scripts/newApp/run-single-quiet.sh
+++ scripts/newApp/run-single-quiet.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-#if [ -f LNSlogfile ]; then
-#mv --backup=numbered LNSlogfile LNSlogfile.save
-#fi
-#if [ -f NSlogfile ]; then
-#mv --backup=numbered NSlogfile NSlogfile.save
-#fi
-#
-java -ea -cp ../../dist/GNS.jar edu.umass.cs.gns.newApp.AppReconfigurableNode -test -nsfile ../../conf/single-server-info -consoleOutputLevel WARNING -fileLoggingLevel WARNING -demandProfileClass edu.umass.cs.gns.newApp.NullDemandProfile  2>&1 | tee NSlogfile &
-java -ea -cp ../../dist/GNS.jar edu.umass.cs.gns.localnameserver.LocalNameServer -nsfile ../../conf/single-server-info -consoleOutputLevel WARNING -fileLoggingLevel WARNING 2>&1 | tee LNSlogfile &
+java -ea -cp ../../dist/GNS.jar edu.umass.cs.gns.newApp.AppReconfigurableNode -test -nsfile ../../conf/single-server-info -consoleOutputLevel WARNING -fileLoggingLevel WARNING -demandProfileClass edu.umass.cs.gns.newApp.NullDemandProfile  2>&1 | tee NSlogfile.txt &
+java -ea -cp ../../dist/GNS.jar edu.umass.cs.gns.localnameserver.LocalNameServer -nsfile ../../conf/single-server-info -consoleOutputLevel WARNING -fileLoggingLevel WARNING 2>&1 | tee LNSlogfile.txt &
