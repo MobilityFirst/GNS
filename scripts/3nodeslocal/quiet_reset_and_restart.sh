37a7708cc4847bc8d54b4182348001ff912697d0
Westy <westy@cs.umass.edu>
2015-07-20 15:24:04 -0400

    more script updates

--- scripts/3nodeslocal/quiet_reset_and_restart.sh
+++ scripts/3nodeslocal/quiet_reset_and_restart.sh
@@ -2,4 +2,4 @@
 ./killem.sh
 java -cp ../../dist/GNS.jar edu.umass.cs.gns.database.MongoRecords -clear
 rm -rf derby.log paxos_logs reconfiguration_DB paxos_large_checkpoints
-./run-nohup.sh
+./run-quiet.sh
