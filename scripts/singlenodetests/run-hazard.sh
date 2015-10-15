5b56120fb478f6fa4758bd64f89e2e2320fe0b3d
David Westbrook <westy@cs.umass.edu>
2015-10-15 14:21:34 -0500

    more hazard updates

--- scripts/singlenodetests/run-hazard.sh
+++ scripts/singlenodetests/run-hazard.sh
@@ -7,5 +7,5 @@ mv --backup=numbered NSlogfile NSlogfile.save
 fi
 #java -ea -cp ../../dist/GNS.jar edu.umass.cs.gns.newApp.AppReconfigurableNode -test -configFile ns_hazard.conf &
 #java -ea -cp ../../dist/GNS.jar edu.umass.cs.gns.localnameserver.LocalNameServer -configFile lns_hazard.conf &
-nohup java -ea -cp ../../dist/GNS.jar edu.umass.cs.gns.gnsApp.AppReconfigurableNode -test -configFile ns_hazard.properties > NSlogfile 2>&1 &
+nohup java -ea -DgigapaxosConfig=../../conf/gigapaxos.gnsApp.properties -cp ../../dist/GNS.jar edu.umass.cs.gns.gnsApp.AppReconfigurableNode -test -configFile ns_hazard.properties > NSlogfile 2>&1 &
 nohup java -ea -cp ../../dist/GNS.jar edu.umass.cs.gns.localnameserver.LocalNameServer -configFile lns_hazard.properties > LNSlogfile 2>&1 &
