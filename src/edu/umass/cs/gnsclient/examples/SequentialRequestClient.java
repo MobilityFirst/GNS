660657e50f3d76f129ddb5f75c289f14f7b5a538
oversky710 <gaozy@cs.umass.edu>
2016-01-04 15:28:38 -0500

    get decode

--- src/edu/umass/cs/gnsclient/examples/SequentialRequestClient.java
+++ src/edu/umass/cs/gnsclient/examples/SequentialRequestClient.java
@@ -72,7 +72,8 @@ public class SequentialRequestClient {
 	    if(flag){
 		    client.activeCodeSet(guid, "read", code64, guidAccount);
 	    }
-	    client.read(guidAccount);
+	    JSONObject jobj = client.read(guidAccount);
+	    System.out.println(jobj);
 	    
 	    for (int i=0; i<5000; i++){
 	    	long t1 = System.nanoTime();
