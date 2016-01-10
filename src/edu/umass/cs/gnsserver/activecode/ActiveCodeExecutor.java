986969b61f696c6507d97605ba02c08c859d2a21
oversky710 <gaozy@cs.umass.edu>
2016-01-10 00:09:09 -0500

    reorder

--- src/edu/umass/cs/gnsserver/activecode/ActiveCodeExecutor.java
+++ src/edu/umass/cs/gnsserver/activecode/ActiveCodeExecutor.java
@@ -53,11 +53,11 @@ public class ActiveCodeExecutor extends ThreadPoolExecutor {
 	
 	
 	@Override
-	public void beforeExecute(Thread t, Runnable r){
-		super.beforeExecute(t, r);
+	public void beforeExecute(Thread t, Runnable r){		
 		if(r instanceof FutureTask<?>){
 			this.guard.registerThread((FutureTask<ValuesMap>) r, t);
-		}			
+		}	
+		super.beforeExecute(t, r);
 	}
 	
 		
@@ -67,7 +67,8 @@ public class ActiveCodeExecutor extends ThreadPoolExecutor {
 	 * https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/ThreadPoolExecutor.html#afterExecute(java.lang.Runnable,%20java.lang.Throwable)
 	 */
         @Override
-	protected void afterExecute(Runnable r, Throwable t) {		
+	protected void afterExecute(Runnable r, Throwable t) {	
+        super.afterExecute(r, t);
 		if (t == null && r instanceof Future<?>) {
 			try {
 				Future<?> future = (Future<?>) r;
@@ -86,7 +87,6 @@ public class ActiveCodeExecutor extends ThreadPoolExecutor {
 		System.out.println("999999999999999999999999999>>>>> Task is about to finish "+r);
 		if(r instanceof FutureTask<?>){
 			this.guard.removeThread((FutureTask<ValuesMap>) r);
-		}
-		super.afterExecute(r, t);
+		}		
 	}
 }
