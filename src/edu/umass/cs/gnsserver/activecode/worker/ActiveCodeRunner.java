a0ec23f45643f9070f654059b802493b5cd067c7
Zhaoyu Gao <gaozy@cs.umass.edu>
2016-02-01 16:46:44 -0500

    debug

--- src/edu/umass/cs/gnsserver/activecode/worker/ActiveCodeRunner.java
+++ src/edu/umass/cs/gnsserver/activecode/worker/ActiveCodeRunner.java
@@ -167,17 +167,15 @@ public class ActiveCodeRunner {
       //engine.eval(code, sc);      
       engine.setContext(sc);
 
-      // ret = (JSONObject) invocable.invokeFunction("run", value, field, querier);
+      ret = (JSONObject) invocable.invokeFunction("run", value, field, querier);
+      /*
       FutureTask<JSONObject> task = new FutureTask<JSONObject>(new ActiveCodeWorkerTask(invocable, value, field, querier));
       executor.execute(task);
-      //ret = task.get(1000, TimeUnit.MILLISECONDS);
-      ret = task.get();
-      
+      ret = task.get(1000, TimeUnit.MILLISECONDS);
+      */
     } catch(ScriptException e){
     	e.printStackTrace();
-    } catch(InterruptedException e) {
-    	e.printStackTrace();
-    } catch(ExecutionException e){
+    } catch(Exception e){
     	e.printStackTrace();
     }
     
