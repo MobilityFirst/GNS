e7492c2f2577a9a9fafcc1dc4218599a86a536e3
Zhaoyu Gao <gaozy@cs.umass.edu>
2016-03-01 16:54:05 -0500

    pass the test case

--- src/edu/umass/cs/gnsserver/activecode/ClientPool.java
+++ src/edu/umass/cs/gnsserver/activecode/ClientPool.java
@@ -131,7 +131,6 @@ public class ClientPool implements Runnable{
 	 * @param t
 	 */
 	protected ActiveCodeClient addClient(Thread t) {
-		long t1 = System.currentTimeMillis();
 		/*
 		 * Invariant: after the client starts, no new thread and client should be created.
 		 */
