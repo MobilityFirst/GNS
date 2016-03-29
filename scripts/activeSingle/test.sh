3d36c5b67c20db9fa16dc2f8d03135afeec5c242
Zhaoyu Gao <gaozy@cs.umass.edu>
2016-03-29 17:02:59 -0400

    capacity test thruput script

---
+++ scripts/activeSingle/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ip=$1
+guid=$2
+benign=$3
+rate=$4
+
+if [ ! -d "result" ]; then
+mkdir result
+fi
+
+echo "Create $2 guids with $3 benign guids on server $1, and send requests at the initial rate of $4 reqs/sec for each guid"
+
+./scripts/client/runSingleClient edu.umass.cs.gnsclient.examples.CreateMultiGuidClient $ip 0 $guid $benign true 2> /tmp/log
+
+for i in {1..10}
+do
+echo "send the requests at the rate $(($i*$2*$4))"
+./scripts/client/runSingleClient edu.umass.cs.gnsclient.examples.CapacityTestClient $ip 0 $guid $(($i*$rate)) $benign &> result/output-$guid-$benign-$(($i*$rate))
+done
+
+echo ""
+
+python process_result.py $guid $benign $rate
