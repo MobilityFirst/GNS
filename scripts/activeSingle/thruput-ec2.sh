a96d5ad8dfe3cb9b8cb029a8f7633b666758d2ed
Zhaoyu Gao <gaozy@cs.umass.edu>
2016-03-29 17:14:48 -0400

    script for throughput

--- scripts/activeSingle/thruput-ec2.sh
+++ scripts/activeSingle/thruput-ec2.sh
@@ -3,6 +3,7 @@ ip=$1
 guid=$2
 benign=$3
 rate=$4
+active=$5
 
 if [ ! -d "result" ]; then
 mkdir result
@@ -10,9 +11,9 @@ fi
 
 echo "Create $2 guids with $3 benign guids on server $1, and send requests at the initial rate of $4 reqs/sec for each guid"
 
-./scripts/client/runEC2Client edu.umass.cs.gnsclient.examples.CreateMultiGuidClient $ip 0 $guid $benign true 2> /tmp/log
+./scripts/client/runEC2Client edu.umass.cs.gnsclient.examples.CreateMultiGuidClient $ip 0 $guid $benign $active 2> /tmp/log
 
-for i in {1..10}
+for i in {1..20}
 do
 echo "send the requests at the rate $(($i*$2*$4))"
 ./scripts/client/runEC2Client edu.umass.cs.gnsclient.examples.CapacityTestClient $ip 0 $guid $(($i*$rate)) $benign &> result/output-$guid-$benign-$(($i*$rate))
