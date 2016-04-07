808a2397db288ed28a4ab8480b1a98d4e77aca0f
Westy <westy@cs.umass.edu>
2016-04-07 17:56:15 -0400

    Still working on MOB-616 (Ensure that all run scripts and gigapaxos properties files work as expected)... more to come...

--- scripts/singlenodetests/run-hazard.sh
+++ scripts/singlenodetests/run-hazard.sh
@@ -8,7 +8,7 @@ IDE_PATH=build/classes:build/test/classes:lib/*:
 #if [ -f NSlogfile ]; then
 #mv --backup=numbered NSlogfile NSlogfile.save
 #fi
-#nohup java -ea -DgigapaxosConfig=conf/gigapaxos.gnsApp.hazard.properties -cp jars/GNS.jar edu.umass.cs.gnsserver.gnsapp.AppReconfigurableNode -test -configFile $SCRIPTS/ns_hazard.properties > NSlogfile 2>&1 &
+#nohup java -ea -DgigapaxosConfig=conf/gigapaxos.server.hazard.properties -cp jars/GNS.jar edu.umass.cs.gnsserver.gnsapp.AppReconfigurableNode -test -configFile $SCRIPTS/ns_hazard.properties > NSlogfile 2>&1 &
 #nohup java -ea -cp jars/GNS.jar edu.umass.cs.gnsserver.localnameserver.LocalNameServer -configFile $SCRIPTS/lns_hazard.properties > LNSlogfile 2>&1 &
 
 
