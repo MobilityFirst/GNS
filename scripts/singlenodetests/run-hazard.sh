42dec70125715d314f341b4b1e8c726e4b5d247c
David Westbrook <westy@cs.umass.edu>
2016-04-08 16:00:35 -0500

    Updated hazard properties

--- scripts/singlenodetests/run-hazard.sh
+++ scripts/singlenodetests/run-hazard.sh
@@ -2,17 +2,17 @@
 SCRIPTS="`dirname \"$0\"`"
 #echo $SCRIPTS
 IDE_PATH=build/classes:build/test/classes:lib/*:
-#if [ -f LNSlogfile ]; then
-#mv --backup=numbered LNSlogfile LNSlogfile.save
-#fi
-#if [ -f NSlogfile ]; then
-#mv --backup=numbered NSlogfile NSlogfile.save
-#fi
-#nohup java -ea -DgigapaxosConfig=conf/gigapaxos.server.hazard.properties -cp jars/GNS.jar edu.umass.cs.gnsserver.gnsapp.AppReconfigurableNode -test -configFile $SCRIPTS/ns_hazard.properties > NSlogfile 2>&1 &
+if [ -f LNSlogfile ]; then
+mv --backup=numbered LNSlogfile LNSlogfile.save
+fi
+if [ -f NSlogfile ]; then
+mv --backup=numbered NSlogfile NSlogfile.save
+fi
+#nohup java -ea -DgigapaxosConfig=conf/gigapaxos.server.hazard.properties -cp jars/GNS.jar edu.umass.cs.gnsserver.gnsapp.AppRecofigurableNode -test -configFile $SCRIPTS/ns_hazard.properties > NSlogfile 2>&1 &
 #nohup java -ea -cp jars/GNS.jar edu.umass.cs.gnsserver.localnameserver.LocalNameServer -configFile $SCRIPTS/lns_hazard.properties > LNSlogfile 2>&1 &
 
 
-java -Xms2048M -ea -cp $IDE_PATH:jars/GNS.jar \
+nohup java -Xms2048M -ea -cp $IDE_PATH:jars/GNS.jar \
 -Djava.util.logging.config.file=logging.properties \
 -DgigapaxosConfig=conf/gigapaxos.server.hazard.properties \
 -Djavax.net.ssl.trustStorePassword=qwerty \
@@ -21,17 +21,17 @@ java -Xms2048M -ea -cp $IDE_PATH:jars/GNS.jar \
 -Djavax.net.ssl.keyStore=conf/keyStore/node100.jks \
 edu.umass.cs.reconfiguration.ReconfigurableNode \
 -test -disableEmailVerification -configFile $SCRIPTS/ns_hazard.properties \
-START_ALL &
+START_ALL > NSlogfile 2>&1 &
 # START_ALL starts all nodes for a single node test; else should
 # explicitly specify nodes as trailing command-line args
 
 # comment to start optional LNS
 #exit
 
-java -ea -DgigapaxosConfig=conf/gigapaxos.server.hazard.properties \
+nohup java -ea -DgigapaxosConfig=conf/gigapaxos.server.hazard.properties \
 -Djavax.net.ssl.trustStorePassword=qwerty \
 -Djavax.net.ssl.trustStore=conf/trustStore/node100.jks \
 -Djavax.net.ssl.keyStorePassword=qwerty \
 -Djavax.net.ssl.keyStore=conf/keyStore/node100.jks -cp jars/GNS.jar \
 edu.umass.cs.gnsserver.localnameserver.LocalNameServer -configFile \
-$SCRIPTS/lns_hazard.properties  &
+$SCRIPTS/lns_hazard.properties > LNSlogfile 2>&1 &
