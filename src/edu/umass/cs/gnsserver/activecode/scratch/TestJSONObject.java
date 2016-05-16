dca52c5f9e6abb071bc0d232106ec1385d0342e1
Zhaoyu Gao <gaozy@cs.umass.edu>
2016-05-16 10:32:49 -0400

    remove experiment clients and scratch code for testing

--- src/edu/umass/cs/gnsserver/activecode/scratch/TestJSONObject.java
+++
@@ -1,9 +0,0 @@
-package edu.umass.cs.gnsserver.activecode.scratch;
-
-import org.json.simple.JSONObject;
-
-public class TestJSONObject {
-	public static void main(String[] args){
-		JSONObject obj = new JSONObject();
-	}
-}
