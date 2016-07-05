497b0eae8c4d984b90048df5a4ae14dce4e0be74
Brendan Teich <bteich@cs.umass.edu>
2016-07-05 18:33:34 +0100

    Added main method to SequentialCreateFieldTimeout bug report.

--- test/edu/umass/cs/gnsclient/client/integrationtests/ServerFailureTests.java
+++ test/edu/umass/cs/gnsclient/client/integrationtests/ServerFailureTests.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -17,28 +17,24 @@ import org.junit.Test;
 import org.junit.rules.TestName;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
 import org.junit.runners.MethodSorters;
 
 import edu.umass.cs.gnsclient.client.GNSClient;
 import edu.umass.cs.gnsclient.client.GNSClientCommands;
 import edu.umass.cs.gnsclient.client.GuidEntry;
-import edu.umass.cs.gnsclient.client.testing.GNSClientCapacityTest;
 import edu.umass.cs.gnsclient.client.util.GuidUtils;
 import edu.umass.cs.gnscommon.utils.RandomString;
 import edu.umass.cs.gnscommon.utils.ThreadUtils;
-import edu.umass.cs.utils.Util;
+import edu.umass.cs.nio.JSONDelayEmulator;
 
 /**
  * @author Brendan
  * 
- * Repeats the Create_Entity test of ServerIntegrationTest a number of times sequentially to try and reproduce a timeout error.
+ * Runs a number of tests while shutting down a minority of GNSApp servers at random intervals.
  *
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class SequentialCreateFieldTimeout {
+public class ServerFailureTests {
 
 	private static final String DEFAULT_ACCOUNT_ALIAS = "support@gns.name";
 
@@ -46,13 +42,40 @@ public class SequentialCreateFieldTimeout {
 	private static final String PASSWORD = "password";
 	private static GNSClientCommands client = null;
 	private static GuidEntry masterGuid;
+	private static ArrayList<String> serverNameArray;
 
-	public static void setAccountAlias(String alias) {
-		accountAlias = alias;
+	public static void causeRandomServerFailure(){
+		//Shutdown a random server, update server count.
+		
+		//Wait a random time length
+		
+		//Restart the server to simulate recovery, update server count.
+		
 	}
-
+	
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		//Testing.ServerList must be a comma separated list of server names that can be run using gpSever.sh
+		String serverList = System.getProperty("Testing.ServerList");
+		if (serverList.equals("")){
+			System.out.println("Missing Testing.ServerList, which must be a comma separated list of server names!");
+			System.exit(1);
+		}
+		while (serverList.length() > 0){
+			int commaIdx = serverList.indexOf(',');
+			if (commaIdx == -1){
+				serverNameArray.add(serverList); //Add the one server still in the input list.
+				break;
+			}
+			else{
+				String server = serverList.substring(0,commaIdx);
+				serverNameArray.add(server); //Add the next server to the ArrayList.
+				serverList = serverList.substring(commaIdx+1,serverList.length()); //Remove that server from the input list.
+			}
+		}
+		
+		
 		client = new GNSClientCommands();
 		// Make all the reads be coordinated
 		client.setForceCoordinatedReads(true);
@@ -79,6 +102,9 @@ public class SequentialCreateFieldTimeout {
 		if (accountCreated == false) {
 			fail("Failure setting up account guid; aborting all tests.");
 		}
+		
+		//Begin emulating transport delays
+		JSONDelayEmulator.emulateDelays();
 	}
 
 	@AfterClass
@@ -115,7 +141,7 @@ public class SequentialCreateFieldTimeout {
 	}
 
 	@Test
-	public void test_010_CreateEntity() throws Exception {
+	public void test_010_CreateEntity() {
 		String runsString = System.getProperty("integrationTest.runs");
 		int numRuns = 1000;
 		if (runsString != null){
@@ -127,25 +153,18 @@ public class SequentialCreateFieldTimeout {
 			}
 			String alias = "testGUID" + RandomString.randomString(12);
 			GuidEntry guidEntry = null;
+			try {
 				guidEntry = GuidUtils.registerGuidWithTestTag(client, masterGuid,
 						alias);
+			} catch (Exception e) {
+				fail("Exception while creating guid on " + Integer.toString(i)+ "th run : " + e);
+			}
 			assertNotNull(guidEntry);
 			assertEquals(alias, guidEntry.getEntityName());
 		}
 	}
 
-	/**
-	 * @param args
-	 * @throws IOException
-	 */
-	public static void main(String[] args) throws IOException {
-		Util.assertAssertionsEnabled();
-		Result result = JUnitCore.runClasses(GNSClientCapacityTest.class);
-		for (Failure failure : result.getFailures()) {
-			System.out.println(failure.getMessage());
-			failure.getException().printStackTrace();
-		}
-	}
+
 
 
 
