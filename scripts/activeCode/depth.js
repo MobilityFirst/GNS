985c6183bc682fcfa484779512f88a3261d87303
Zhaoyu Gao <gaozy@cs.umass.edu>
2016-07-15 15:07:58 -0400

    script should use java JSON operation instead of javascript

--- scripts/activeCode/depth.js
+++ scripts/activeCode/depth.js
@@ -1,6 +1,6 @@
 function run(value, field, querier){
-    var depth = querier.readGuid(null,"activeField");
-    depth = depth -1;
+    var map = querier.readGuid(null,"activeField");
+    var depth = map.getInt("activeField") - 1;
     while(depth>0){
     	querier.readGuid(null,"activeField");
     	depth--;
