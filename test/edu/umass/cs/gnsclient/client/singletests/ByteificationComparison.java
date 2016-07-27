449cb3d2827d6f828f69ab0ed388e270f432f8a4
Brendan Teich <bteich@cs.umass.edu>
2016-07-27 18:21:57 +0100

    Added multiple Json to bytes methods in gnscommon.utils.JSONByteConverter and added a test to compare them that can be run with ant runtest -Dtest="ByteificationComparison".  Results are being posted on JIRA under the serialization task.

--- test/edu/umass/cs/gnsclient/client/singletests/ByteificationComparison.java
+++ test/edu/umass/cs/gnsclient/client/singletests/ByteificationComparison.java
@@ -17,6 +17,9 @@ import org.json.JSONObject;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import edu.umass.cs.gnscommon.utils.JSONByteConverter;
 import edu.umass.cs.gnsserver.gnsapp.packet.CommandPacket;
 import edu.umass.cs.utils.Util;
 
@@ -51,127 +54,6 @@ public class ByteificationComparison {
 		testJson.put("otherCollection", collection3);
 		
 	}
-	public byte[] recursiveCollectionToBytes(JSONArray c) throws JSONException, IOException{
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		int len1 = c.length();
-		out.write(COLLECTION_IDENTIFIER);
-		out.write(len1);
-		for (int i = 0; i < len1; i++){
-			Object obj = c.get(i);
-			if ( obj instanceof JSONArray){
-				out.write(recursiveCollectionToBytes((JSONArray)obj));
-			}
-			else{
-				byte[] stringBytes = ((String)obj).getBytes();
-				int len2 = stringBytes.length;
-				out.write(STRING_IDENTIFIER);
-				out.write(len2);
-				out.write(stringBytes);
-			}
-
-		}
-		return out.toByteArray();
-		
-	}
-	
-	public byte[] recursiveToBytes(JSONObject json) throws JSONException, IOException{
-		Iterator<String> keys = json.keys();
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		while (keys.hasNext()){
-			String key = keys.next();
-			Object obj = json.get(key);
-			int len1 = key.length();
-			out.write(STRING_IDENTIFIER);
-			out.write(len1);
-			out.write(key.getBytes());
-			if ( obj instanceof JSONArray){
-				out.write(recursiveCollectionToBytes((JSONArray)obj));
-			}
-			else{
-				byte[] stringBytes = ((String)obj).getBytes();
-				int len2 = stringBytes.length;
-				out.write(STRING_IDENTIFIER);
-				out.write(len2);
-				out.write(stringBytes);
-			}
-
-			}
-		return out.toByteArray();
-	}
-	
-	/**
-	 * 
-	 * @param bytes
-	 * @param startOffset startOffset Where in the byte array the int that signifies the length of the String in bytes begins.
-	 * @return
-	 */
-	public String stringFromBytes(byte[] bytes, int startOffset){
-		int len = (int) (bytes[startOffset] << 24) & (bytes[startOffset+1] << 16) & (bytes[startOffset+2] << 8) & bytes[startOffset+3];
-			byte[] stringBytes = new byte[len];
-			//First 4 bytes after startOffset are length
-			int offset = startOffset+4;
-			for (int i = 0; i < len; i++){
-				stringBytes[i] = bytes[offset+i];
-			}
-			return new String(stringBytes);
-		
-	}
-	/**
-	 * 
-	 * @param bytes
-	 * @param startOffset Where in the byte array the int that signifies the length of the collection in elements begins.
-	 * @return
-	 */
-	public Collection<?> collectionFromBytes(byte[] bytes, int startOffset){
-		Collection<Object> c = new ArrayList<Object>();
-		int len = (int) (bytes[startOffset] << 24) & (bytes[startOffset+1] << 16) & (bytes[startOffset+2] << 8) & bytes[startOffset+3];
-		int offset = startOffset+4;//Start after first 4 bytes, which are the length.
-		for (int i = 0; i < len; i++){
-			short identifier = (short) ((bytes[offset] << 8) & bytes[offset+1]);
-			if (identifier == COLLECTION_IDENTIFIER){
-				Collection<?> newCollection = collectionFromBytes(bytes, offset+2);
-				c.add(newCollection);
-			}
-			else if (identifier == STRING_IDENTIFIER){
-				String newString = stringFromBytes(bytes, offset+2);
-				c.add(newString);
-			}
-			else{
-				fail("Invalid identifier during reconstruction of " + bytes.toString() + " at index " + Integer.toString(offset));
-			}
-		}
-		return c;
-	}
-	
-	public JSONObject recreateRecursive(byte[] bytes) throws JSONException{
-		JSONObject json = new JSONObject();
-		//Assuming Big Endian byte representation, pattern is 2 bytes identifier, 4 bytes length integer.
-		short identifier = (short) ((bytes[0] << 8) & bytes[1]);
-		int len = (int) (bytes[2] << 24) & (bytes[3] << 16) & (bytes[4] << 8) & bytes[5];
-		
-		//First 2 bytes are the type
-		String key = stringFromBytes(bytes, 2);
-		//First 6 bytes are array type and length for the key, next "len" are the key, next 2 are next identifier for the value.
-		int offset = 6+len;
-		identifier = (short) ((bytes[offset] << 8) & bytes[offset+1]);
-
-		offset = offset+2; //Move offset to after the identifier.			
-		switch(identifier){
-		case STRING_IDENTIFIER:
-			String value = stringFromBytes(bytes, offset);
-			json.put(key, value);
-			break;
-		case COLLECTION_IDENTIFIER:
-			Collection<?> collection = collectionFromBytes(bytes, offset);
-			json.put(key, collection);
-			break;
-		}
-
-		
-		return json;
-	}
-	
-
 	@Test
 	public void test_01_JSON_Default() throws JSONException {
 		long startTime = System.nanoTime();
@@ -186,27 +68,71 @@ public class ByteificationComparison {
 		
 		byte[] bytes = testJson.toString().getBytes();
 		JSONObject testJson2 = new JSONObject(new String(bytes));
-		System.out.println("JSON1: \n" + testJson.toString());
-		System.out.println("JSON2: \n" + testJson2.toString());
+		assert(testJson.toString().equals(testJson2.toString()));
+		//System.out.println("JSON1: \n" + testJson.toString());
+		//System.out.println("JSON2: \n" + testJson2.toString());
 		
 	}
 	
 	@Test
-	public void test_02_recursive() throws JSONException, IOException {
+	public void test_02_hardcoded() throws JSONException, IOException {
 		long startTime = System.nanoTime();
 		for (int i = 0; i < TEST_RUNS; i++){
-			byte[] bytes = recursiveToBytes(testJson);
-			JSONObject testJson2 = recreateRecursive(bytes);
+			byte[] bytes = JSONByteConverter.toBytesHardcoded(testJson);
+			JSONObject testJson2 = JSONByteConverter.fromBytesHardcoded(bytes);
 			//assert(testJson.equals(testJson2));
 		}
 		long endTime = System.nanoTime();
 		double avg = (endTime - startTime) / (TEST_RUNS);
-		System.out.println("Average byteification time RECURSIVE was " + avg + " nanoseconds.");
+		System.out.println("Average byteification time HARDCODED was " + avg + " nanoseconds.");
 		
-		byte[] bytes = testJson.toString().getBytes();
-		JSONObject testJson2 = new JSONObject(new String(bytes));
+		byte[] bytes = JSONByteConverter.toBytesHardcoded(testJson);
+		JSONObject testJson2 = JSONByteConverter.fromBytesHardcoded(bytes);
+		assert(testJson.toString().equals(testJson2.toString()));
+		//System.out.println("JSON1: \n" + testJson.toString());
+		//System.out.println("JSON2: \n" + testJson2.toString());
+	}
+	
+	@Test
+	public void test_03_jackson() throws IOException, JSONException{
+		long startTime = System.nanoTime();
+		JSONObject testJson2;
+		for (int i = 0; i < TEST_RUNS; i++){
+			byte[] bytes = JSONByteConverter.toBytesJackson(testJson);
+			testJson2 = JSONByteConverter.fromBytesJackson(bytes);
+			//assert(testJson.equals(testJson2));
+		}
+		long endTime = System.nanoTime();
+		double avg = (endTime - startTime) / (TEST_RUNS);
+		System.out.println("Average byteification time Jackson was " + avg + " nanoseconds.");
+		byte[] bytes = JSONByteConverter.toBytesJackson(testJson);
+		testJson2 = JSONByteConverter.fromBytesJackson(bytes);
+
+		//System.out.println("JSON1: \n" + testJson.toString());
+		//System.out.println("JSON2: \n" + testJson2.toString());
+		//assert(testJson.toString().equals(testJson2.toString()));
+	}
+	
+	@Test
+	public void test_04_msgpack() throws IOException, JSONException{
+		long startTime = System.nanoTime();
+		JSONObject testJson2;
+		for (int i = 0; i < TEST_RUNS; i++){
+			byte[] bytes = JSONByteConverter.toBytesMsgpack(testJson);
+			testJson2 = JSONByteConverter.fromBytesMsgpack(bytes);
+			//assert(testJson.equals(testJson2));
+		}
+		long endTime = System.nanoTime();
+		double avg = (endTime - startTime) / (TEST_RUNS);
+		System.out.println("Average byteification time Jackson was " + avg + " nanoseconds.");
+		byte[] bytes = JSONByteConverter.toBytesMsgpack(testJson);
+		testJson2 = JSONByteConverter.fromBytesMsgpack(bytes);
+
 		System.out.println("JSON1: \n" + testJson.toString());
 		System.out.println("JSON2: \n" + testJson2.toString());
+		assert(testJson.toString().equals(testJson2.toString()));
 	}
+	
+	
 
 }
