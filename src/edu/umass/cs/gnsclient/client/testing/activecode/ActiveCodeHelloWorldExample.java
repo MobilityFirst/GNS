fed71e037e995b8752c922991431747a3c4b5b20
Zhaoyu Gao <gaozy@cs.umass.edu>
2016-08-16 19:22:04 -0400

    add tests for ActiveClient and hello world client back

---
+++ src/edu/umass/cs/gnsclient/client/testing/activecode/ActiveCodeHelloWorldExample.java
@@ -0,0 +1,94 @@
+package edu.umass.cs.gnsclient.client.testing.activecode;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import edu.umass.cs.gnsclient.client.GNSClientCommands;
+import edu.umass.cs.gnsclient.client.util.GuidUtils;
+import edu.umass.cs.gnsserver.gnsapp.clientCommandProcessor.commandSupport.ActiveCode;
+
+/**
+ * This is an hello world example for ActiveGNS.
+ * In this example, it shows you how to create an account, create a field
+ * on the account, retrieve the value of the field, and deploy your own
+ * code on ActiveGNS.
+ * 
+ * @author gaozy
+ *
+ */
+public class ActiveCodeHelloWorldExample {
+		
+	
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		
+		boolean update = true;
+		if(System.getProperty("update")!=null){
+			update = Boolean.parseBoolean(System.getProperty("update"));
+		}
+		
+		boolean isRead = true;
+		if(System.getProperty("isRead")!=null){
+			isRead = Boolean.parseBoolean(System.getProperty("isRead"));
+		}
+		
+		String codeFile = "scripts/activeCode/noop.js";
+		if(args.length > 0){
+			codeFile = args[0];
+		}
+				
+		// create a client
+		final GNSClientCommands client = new GNSClientCommands();
+		
+		final String ACCOUNT_GUID_PREFIX = "GNS_ACCOUNT";
+		final String PASSWORD = "";
+		
+		// create an account
+		final edu.umass.cs.gnsclient.client.util.GuidEntry entry = GuidUtils.lookupOrCreateAccountGuid(client, ACCOUNT_GUID_PREFIX, PASSWORD);
+		
+		String field = "someField";
+		String value = "someValue";
+		
+		String depth_field = "depthField";
+		String depth_result = "Depth query succeeds";
+		
+		// set up a field
+		client.fieldUpdate(entry,  field, value);
+		client.fieldUpdate(entry, depth_field, depth_result);
+		
+		// get the value of the field
+		String response = client.fieldRead(entry, field);
+		
+		System.out.println("Before the code is deployed, the value of field("+field+") is "+response);
+		
+		// read in the code as a string
+		final String code = new String(Files.readAllBytes(Paths.get(codeFile)));
+		
+		// set up the code for on read operation
+		if(isRead){
+			client.activeCodeClear(entry.getGuid(), ActiveCode.READ_ACTION, entry);
+			if(update)
+				client.activeCodeSet(entry.getGuid(), ActiveCode.READ_ACTION, code, entry);
+		} else {
+			client.activeCodeClear(entry.getGuid(), ActiveCode.WRITE_ACTION, entry);
+			if(update)
+				client.activeCodeSet(entry.getGuid(), ActiveCode.WRITE_ACTION, code, entry);
+		}
+		// get the value of the field again
+		response = client.fieldRead(entry, field);
+		System.out.println("After the code is deployed, the value of field("+field+") is "+response);
+		
+		ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(new File("guid")));
+		entry.writeObject(output);
+		output.flush();
+		output.close();
+		
+		System.exit(0);
+	}
+}
---
+++ test/edu/umass/cs/gnsserver/activecode/prototype/ActiveClientSequentialTest.java
@@ -0,0 +1,100 @@
+package edu.umass.cs.gnsserver.activecode.prototype;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.ExecutionException;
+
+import org.json.JSONException;
+import org.junit.Test;
+
+import edu.umass.cs.gnsserver.utils.ValuesMap;
+
+/**
+ * @author gaozy
+ *
+ */
+public class ActiveClientSequentialTest {
+
+	
+	/**
+	 * @throws JSONException
+	 * @throws ActiveException
+	 * @throws InterruptedException
+	 * @throws ExecutionException
+	 */
+	@Test
+	public void test_sequentialRequestThroughput() throws JSONException, ActiveException, InterruptedException, ExecutionException {
+		
+		final String suffix = "";
+		
+		int numThread = 1;
+		if(System.getProperty("numThread")!=null){
+			numThread = Integer.parseInt(System.getProperty("numThread"));
+		}
+		
+		long budget = 50;
+		if(System.getProperty("budget")!= null){
+			budget = Long.parseLong(System.getProperty("budget"));
+		}
+		
+		long executionTime = 100;
+		if(System.getProperty("executionTime")!=null){
+			executionTime = Long.parseLong(System.getProperty("executionTime"));
+		}
+		
+		String codeFile = "./scripts/activeCode/wasteTime.js";
+		if(System.getProperty("codeFile")!=null){
+			codeFile = System.getProperty("codeFile");
+		}
+		
+		String cfile = "/tmp/client"+suffix;
+		String sfile = "/tmp/server"+suffix;		
+		/**
+		 * Test client performance with named pipe channel
+		 */
+		ActiveNonBlockingClient client = new ActiveNonBlockingClient(null, cfile, sfile, 0, numThread);
+		Thread th = new Thread(client);
+		th.start();
+		
+		String guid = "guid";
+		String field = "name";
+		String code = "";
+		try {
+			code = new String(Files.readAllBytes(Paths.get(codeFile)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		ValuesMap value = new ValuesMap();
+		value.put(field, executionTime);	
+		ValuesMap result = client.runCode(null, guid, field, code, value, 0, 1000);
+
+		assertEquals(result.toString(), value.toString());
+		
+		int initial = client.getRecv();
+		
+		int n = 1000;
+		
+		long t1 = System.currentTimeMillis();
+		
+		for (int i=0; i<n; i++){
+			client.runCode(null, guid, field, code, value, 0, budget);
+			
+		}
+		
+		long elapsed = System.currentTimeMillis() - t1;
+		System.out.println("It takes "+elapsed+"ms");
+		System.out.println("The average time for each task is "+elapsed/n+"ms.");
+		assert(elapsed/n-budget<budget*0.1);
+		assert(client.getRecv()-initial == n):"the number of responses is not the same as the number of requests";
+		
+		System.out.println("Sequential throughput test succeeds.");
+		
+		th.interrupt();
+		
+		client.shutdown();
+		
+	}
+}
