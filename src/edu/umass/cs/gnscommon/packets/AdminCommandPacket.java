f6d83659ff6a15e2603a36162d3b0c39f64de609
Brendan Teich <bteich@umass.edu>
2016-08-29 00:54:34 +0100

    Created AdminCommandPacket, which has PacketType ADMIN_COMMAND and asserts that it's commands are of type MUTUAL_AUTH.

---
+++ src/edu/umass/cs/gnscommon/packets/AdminCommandPacket.java
@@ -0,0 +1,31 @@
+package edu.umass.cs.gnscommon.packets;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.umass.cs.gnscommon.CommandType;
+import edu.umass.cs.gnsserver.gnsapp.packet.Packet.PacketType;
+import edu.umass.cs.reconfiguration.reconfigurationutils.RequestParseException;
+
+public class AdminCommandPacket extends CommandPacket {
+
+	public AdminCommandPacket(long requestId, JSONObject command) {
+		super(requestId, command);
+		this.setType(PacketType.ADMIN_COMMAND);
+		assert(this.getCommandType().isMutualAuth());
+	}
+
+	public AdminCommandPacket(JSONObject json) throws JSONException {
+		super(json);
+		this.setType(PacketType.ADMIN_COMMAND);
+		assert(this.getCommandType().isMutualAuth());
+	}
+
+	public AdminCommandPacket(byte[] bytes) throws RequestParseException {
+		super(bytes);
+		this.setType(PacketType.ADMIN_COMMAND);
+		assert(this.getCommandType().isMutualAuth());
+	}
+
+
+}
