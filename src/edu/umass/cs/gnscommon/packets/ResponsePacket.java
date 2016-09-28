165f7c8563494966d618662366a214da2f0e73b4
Westy <westy@cs.umass.edu>
2016-09-28 19:23:14 -0400

    Backward compatible support for older ios clients

--- src/edu/umass/cs/gnscommon/packets/ResponsePacket.java
+++ src/edu/umass/cs/gnscommon/packets/ResponsePacket.java
@@ -22,13 +22,11 @@ import edu.umass.cs.gnsserver.gnsapp.clientCommandProcessor.commandSupport.Comma
 import edu.umass.cs.gnsserver.gnsapp.packet.BasicPacketWithClientAddress;
 import edu.umass.cs.gnsserver.gnsapp.packet.Packet;
 import edu.umass.cs.gnsserver.gnsapp.packet.Packet.PacketType;
-import edu.umass.cs.gnsserver.main.GNSConfig;
 import edu.umass.cs.nio.MessageNIOTransport;
 import edu.umass.cs.utils.Util;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
-import java.util.logging.Level;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -40,280 +38,308 @@ import org.json.JSONObject;
  *
  */
 public class ResponsePacket extends BasicPacketWithClientAddress
-		implements ClientRequest {
+        implements ClientRequest {
 
-	private final static String QID = GNSProtocol.REQUEST_ID.toString();
-	private final static String NAME = GNSProtocol.SERVICE_NAME.toString();
-	private final static String RETVAL = GNSProtocol.RETURN_VALUE.toString();
-	private final static String ERRCODE = GNSProtocol.ERROR_CODE.toString();
+  private final static String QID = GNSProtocol.REQUEST_ID.toString();
+  private final static String NAME = GNSProtocol.SERVICE_NAME.toString();
+  private final static String RETVAL = GNSProtocol.RETURN_VALUE.toString();
+  private final static String ERRCODE = GNSProtocol.ERROR_CODE.toString();
 
-	/**
-	 * Identifier of the request.
-	 */
-	private long clientRequestId;
-	/**
-	 * The service name from the request. Usually the guid or HRN.
-	 */
-	private final String serviceName;
-	/**
-	 * The returned value.
-	 */
-	private final String returnValue;
-	/**
-	 * Indicates if the response is an error.
-	 */
-	private final GNSResponseCode errorCode;
+  private final static boolean SUPPORT_OLD_PROTOCOL = true;
+  private final static String OLD_COMMAND_RETURN_PACKET_REQUESTID = "clientreqID";
+  private final static String OLD_COMMAND_RETURN_PACKET_RETURNVALUE = "returnValue";
+  private final static String OLD_COMMAND_RETURN_PACKET_ERRORCODE = "errorCode";
 
-	/**
-	 * Creates a CommandValueReturnPacket from a CommandResponse.
-	 *
-	 * @param requestId
-	 * @param serviceName
-	 * @param response
-	 * @param requestCnt
-	 *            - current number of requests handled by the CCP (can be used
-	 *            to tell how busy CCP is)
-	 * @param requestRate
-	 * @param cppProccessingTime
-	 */
-	public ResponsePacket(long requestId, String serviceName,
-			CommandResponse response, long requestCnt, int requestRate,
-			long cppProccessingTime) {
-		this.setType(PacketType.COMMAND_RETURN_VALUE);
-		this.clientRequestId = requestId;
-		this.serviceName = serviceName;
-		this.returnValue = response.getReturnValue();
-		this.errorCode = response.getExceptionOrErrorCode();
-	}
+  /**
+   * Identifier of the request.
+   */
+  private long clientRequestId;
+  /**
+   * The service name from the request. Usually the guid or HRN.
+   */
+  private final String serviceName;
+  /**
+   * The returned value.
+   */
+  private final String returnValue;
+  /**
+   * Indicates if the response is an error.
+   */
+  private final GNSResponseCode errorCode;
 
-	/**
-	 * @param serviceName
-	 * @param requestId
-	 * @param code
-	 * @param returnValue
-	 */
-	public ResponsePacket(String serviceName, long requestId,
-			GNSResponseCode code, String returnValue) {
-		this.setType(PacketType.COMMAND_RETURN_VALUE);
-		this.clientRequestId = requestId;
-		this.serviceName = serviceName;
-		this.returnValue = returnValue;
-		this.errorCode = code;
-	}
+  /**
+   * Creates a CommandValueReturnPacket from a CommandResponse.
+   *
+   * @param requestId
+   * @param serviceName
+   * @param response
+   * @param requestCnt
+   * - current number of requests handled by the CCP (can be used
+   * to tell how busy CCP is)
+   * @param requestRate
+   * @param cppProccessingTime
+   */
+  public ResponsePacket(long requestId, String serviceName,
+          CommandResponse response, long requestCnt, int requestRate,
+          long cppProccessingTime) {
+    this.setType(PacketType.COMMAND_RETURN_VALUE);
+    this.clientRequestId = requestId;
+    this.serviceName = serviceName;
+    this.returnValue = response.getReturnValue();
+    this.errorCode = response.getExceptionOrErrorCode();
+  }
 
-	/**
-	 * Creates a CommandValueReturnPacket from a JSONObject.
-	 *
-	 * @param json
-	 * @throws JSONException
-	 */
-	public ResponsePacket(JSONObject json) throws JSONException {
-		this.type = Packet.getPacketType(json);
-		this.clientRequestId = json.getLong(QID);
-		this.serviceName = json.getString(NAME);
-		this.returnValue = json.getString(RETVAL);
-		if (json.has(ERRCODE)) {
-			this.errorCode = GNSResponseCode.getResponseCode(json
-					.getInt(ERRCODE));
-		} else {
-			this.errorCode = GNSResponseCode.NO_ERROR;
-		}
-	}
+  /**
+   * @param serviceName
+   * @param requestId
+   * @param code
+   * @param returnValue
+   */
+  public ResponsePacket(String serviceName, long requestId,
+          GNSResponseCode code, String returnValue) {
+    this.setType(PacketType.COMMAND_RETURN_VALUE);
+    this.clientRequestId = requestId;
+    this.serviceName = serviceName;
+    this.returnValue = returnValue;
+    this.errorCode = code;
+  }
 
-	/**
-	 * This method is used by fromBytes to recreate a CommandValueReturnPacket
-	 * from a byte array.
-	 * 
-	 * @param requestId
-	 * @param errorNumber
-	 * @param serviceName
-	 * @param responseValue
-	 */
-	public ResponsePacket(long requestId, int errorNumber,
-			String serviceName, String responseValue) {
-		this.setType(PacketType.COMMAND_RETURN_VALUE);
-		this.clientRequestId = requestId;
-		this.serviceName = serviceName;
-		this.returnValue = responseValue;
-		this.errorCode = GNSResponseCode.getResponseCode(errorNumber);
-	}
+  /**
+   * Creates a CommandValueReturnPacket from a JSONObject.
+   *
+   * @param json
+   * @throws JSONException
+   */
+  public ResponsePacket(JSONObject json) throws JSONException {
+    this.type = Packet.getPacketType(json);
 
-	/**
-	 * Converts the command object into a JSONObject.
-	 *
-	 * @return a JSONObject
-	 * @throws org.json.JSONException
-	 */
-	@Override
-	public JSONObject toJSONObject() throws JSONException {
-		JSONObject json = new JSONObject();
-		Packet.putPacketType(json, getType());
-		json.put(QID, this.clientRequestId);
-		json.put(NAME, this.serviceName);
-		json.put(RETVAL, returnValue);
-		if (errorCode != null) {
-			json.put(ERRCODE, errorCode.getCodeValue());
-		} else {
-			json.put(ERRCODE, GNSResponseCode.NO_ERROR.getCodeValue());
-		}
-		return json;
-	}
+    if (!SUPPORT_OLD_PROTOCOL) {
+      this.clientRequestId = json.getLong(QID);
+      this.returnValue = json.getString(RETVAL);
+      this.serviceName = json.getString(NAME);
+      if (json.has(ERRCODE)) {
+        this.errorCode = GNSResponseCode.getResponseCode(json.getInt(ERRCODE));
+      } else {
+        this.errorCode = GNSResponseCode.NO_ERROR;
+      }
+      // probably not necessary as the old ios client never actually sends these 
+      // but just to be thorough
+    } else {
+      if (json.has(QID)) {
+        this.clientRequestId = json.getLong(QID);
+      } else if (json.has(OLD_COMMAND_RETURN_PACKET_REQUESTID)) {
+        this.clientRequestId = json.getLong(OLD_COMMAND_RETURN_PACKET_REQUESTID);
+      } else {
+        throw new JSONException("Packet missing field " + QID);
+      }
+      if (json.has(RETVAL)) {
+        this.returnValue = json.getString(RETVAL);
+      } else if (json.has(OLD_COMMAND_RETURN_PACKET_RETURNVALUE)) {
+        this.returnValue = json.getString(OLD_COMMAND_RETURN_PACKET_RETURNVALUE);
+      } else {
+        throw new JSONException("Packet missing field " + RETVAL);
+      }
+      if (json.has(ERRCODE)) {
+        this.errorCode = GNSResponseCode.getResponseCode(json.getInt(ERRCODE));
+      } else if (json.has(OLD_COMMAND_RETURN_PACKET_ERRORCODE)) {
+        this.errorCode = GNSResponseCode.getResponseCode(json.getInt(OLD_COMMAND_RETURN_PACKET_ERRORCODE));
+      } else {
+        this.errorCode = GNSResponseCode.NO_ERROR;
+      }
+      // not sure what to do here; this is nothing in the old protocol for this from the ios client
+      this.serviceName = json.optString(NAME, "unknown");
+    }
+  }
 
-	/**
-	 * Converts the CommandValueReturnPacket to a byte array.
-	 * 
-	 * @return The byte array
-	 */
-	public byte[] toBytes() {
-		try {
+  /**
+   * This method is used by fromBytes to recreate a CommandValueReturnPacket
+   * from a byte array.
+   *
+   * @param requestId
+   * @param errorNumber
+   * @param serviceName
+   * @param responseValue
+   */
+  public ResponsePacket(long requestId, int errorNumber,
+          String serviceName, String responseValue) {
+    this.setType(PacketType.COMMAND_RETURN_VALUE);
+    this.clientRequestId = requestId;
+    this.serviceName = serviceName;
+    this.returnValue = responseValue;
+    this.errorCode = GNSResponseCode.getResponseCode(errorNumber);
+  }
 
-			// We need to include the following fields in our byte array:
+  /**
+   * Converts the command object into a JSONObject.
+   *
+   * @return a JSONObject
+   * @throws org.json.JSONException
+   */
+  @Override
+  public JSONObject toJSONObject() throws JSONException {
+    JSONObject json = new JSONObject();
+    Packet.putPacketType(json, getType());
+    json.put(QID, this.clientRequestId);
+    json.put(NAME, this.serviceName);
+    json.put(RETVAL, returnValue);
+    if (errorCode != null) {
+      json.put(ERRCODE, errorCode.getCodeValue());
+    } else {
+      json.put(ERRCODE, GNSResponseCode.NO_ERROR.getCodeValue());
+    }
+    return json;
+  }
 
-			// private long clientRequestId; - 8 bytes
-			// private long LNSRequestId; - 8 bytes
-			// private final GNSResponseCode errorCode; - Represented by an
-			// integer - 4 bytes
-			// serviceName String's length - an int - 4 bytes
-			// private final String serviceName; - variable length
-			// returnValue String's length - ant int - 4 bytes
-			// private final String returnValue; - variable length
-			int errorCodeInt = errorCode.getCodeValue();
-			byte[] serviceNameBytes = serviceName
-					.getBytes(MessageNIOTransport.NIO_CHARSET_ENCODING);
-			byte[] returnValueBytes = returnValue
-					.getBytes(MessageNIOTransport.NIO_CHARSET_ENCODING);
+  /**
+   * Converts the CommandValueReturnPacket to a byte array.
+   *
+   * @return The byte array
+   */
+  public byte[] toBytes() {
+    try {
 
-			ByteBuffer buf = ByteBuffer.allocate(
-			// requestID
-					Long.BYTES
-					// error code
-							+ Integer.BYTES
-							// name length
-							+ Integer.BYTES
-							// name bytes
-							+ serviceNameBytes.length
-							// returnValue length
-							+ Integer.BYTES
-							// returnValue bytes
-							+ returnValueBytes.length);
+      // We need to include the following fields in our byte array:
+      // private long clientRequestId; - 8 bytes
+      // private long LNSRequestId; - 8 bytes
+      // private final GNSResponseCode errorCode; - Represented by an
+      // integer - 4 bytes
+      // serviceName String's length - an int - 4 bytes
+      // private final String serviceName; - variable length
+      // returnValue String's length - ant int - 4 bytes
+      // private final String returnValue; - variable length
+      int errorCodeInt = errorCode.getCodeValue();
+      byte[] serviceNameBytes = serviceName
+              .getBytes(MessageNIOTransport.NIO_CHARSET_ENCODING);
+      byte[] returnValueBytes = returnValue
+              .getBytes(MessageNIOTransport.NIO_CHARSET_ENCODING);
 
-			// requestID
-			buf.putLong(clientRequestId)
+      ByteBuffer buf = ByteBuffer.allocate(
+              // requestID
+              Long.BYTES
+              // error code
+              + Integer.BYTES
+              // name length
+              + Integer.BYTES
+              // name bytes
+              + serviceNameBytes.length
+              // returnValue length
+              + Integer.BYTES
+              // returnValue bytes
+              + returnValueBytes.length);
 
-			// error code
-					.putInt(errorCodeInt)
-					// name length
+      // requestID
+      buf.putLong(clientRequestId)
+              // error code
+              .putInt(errorCodeInt)
+              // name length
 
-					// name bytes
-					.putInt(serviceNameBytes.length)
+              // name bytes
+              .putInt(serviceNameBytes.length)
+              .put(serviceNameBytes)
+              // returnValue length
+              .putInt(returnValueBytes.length)
+              // returnValue bytes
+              .put(returnValueBytes);
 
-					.put(serviceNameBytes)
+      return buf.array();
+    } catch (UnsupportedEncodingException e) {
+      throw new RuntimeException(e);
+    }
 
-					// returnValue length
-					.putInt(returnValueBytes.length)
+  }
 
-					// returnValue bytes
-					.put(returnValueBytes);
+  /**
+   * Constructs a CommandValueReturnPacket from a byte array.
+   *
+   * @param bytes
+   * The byte array created by the toBytes method of a
+   * CommandValueReturnPacket
+   * @return The CommandValueReturnPacket represented by the bytes
+   * @throws UnsupportedEncodingException
+   */
+  public static final ResponsePacket fromBytes(byte[] bytes)
+          throws UnsupportedEncodingException {
+    ByteBuffer buf = ByteBuffer.wrap(bytes);
+    long clientReqId = buf.getLong();
+    int errorCodeInt = buf.getInt();
+    int serviceNameLength = buf.getInt();
+    byte[] serviceNameBytes = new byte[serviceNameLength];
+    buf.get(serviceNameBytes);
+    String serviceNameString = new String(serviceNameBytes,
+            MessageNIOTransport.NIO_CHARSET_ENCODING);
+    int returnValueLength = buf.getInt();
+    byte[] returnValueBytes = new byte[returnValueLength];
+    buf.get(returnValueBytes);
+    String returnValueString = new String(returnValueBytes,
+            MessageNIOTransport.NIO_CHARSET_ENCODING);
+    return new ResponsePacket(clientReqId, errorCodeInt,
+            serviceNameString, returnValueString);
 
-			return buf.array();
-		} catch (UnsupportedEncodingException e) {
-			throw new RuntimeException(e);
-		}
+  }
 
-	}
+  /**
+   * Get the client request id.
+   *
+   * @return the client request id
+   */
+  public long getClientRequestId() {
+    return clientRequestId;
+  }
 
-	/**
-	 * Constructs a CommandValueReturnPacket from a byte array.
-	 * 
-	 * @param bytes
-	 *            The byte array created by the toBytes method of a
-	 *            CommandValueReturnPacket
-	 * @return The CommandValueReturnPacket represented by the bytes
-	 * @throws UnsupportedEncodingException
-	 */
-	public static final ResponsePacket fromBytes(byte[] bytes)
-			throws UnsupportedEncodingException {
-		ByteBuffer buf = ByteBuffer.wrap(bytes);
-		long clientReqId = buf.getLong();
-		int errorCodeInt = buf.getInt();
-		int serviceNameLength = buf.getInt();
-		byte[] serviceNameBytes = new byte[serviceNameLength];
-		buf.get(serviceNameBytes);
-		String serviceNameString = new String(serviceNameBytes,
-				MessageNIOTransport.NIO_CHARSET_ENCODING);
-		int returnValueLength = buf.getInt();
-		byte[] returnValueBytes = new byte[returnValueLength];
-		buf.get(returnValueBytes);
-		String returnValueString = new String(returnValueBytes,
-				MessageNIOTransport.NIO_CHARSET_ENCODING);
-		return new ResponsePacket(clientReqId, errorCodeInt,
-				serviceNameString, returnValueString);
+  @Override
+  public String getServiceName() {
+    return serviceName;
+  }
 
-	}
+  /**
+   * Get the return value.
+   *
+   * @return the return value
+   */
+  public String getReturnValue() {
+    return returnValue;
+  }
 
-	/**
-	 * Get the client request id.
-	 *
-	 * @return the client request id
-	 */
-	public long getClientRequestId() {
-		return clientRequestId;
-	}
+  /**
+   * Get the error code.
+   *
+   * @return the error code
+   */
+  public GNSResponseCode getErrorCode() {
+    return errorCode;
+  }
 
-	@Override
-	public String getServiceName() {
-		return serviceName;
-	}
+  @Override
+  public ClientRequest getResponse() {
+    return this.response;
+  }
 
-	/**
-	 * Get the return value.
-	 *
-	 * @return the return value
-	 */
-	public String getReturnValue() {
-		return returnValue;
-	}
+  @Override
+  public long getRequestID() {
+    return clientRequestId;
+  }
 
-	/**
-	 * Get the error code.
-	 *
-	 * @return the error code
-	 */
-	public GNSResponseCode getErrorCode() {
-		return errorCode;
-	}
+  @Override
+  public Object getSummary() {
+    return new Object() {
+      @Override
+      public String toString() {
+        return getRequestType() + ":" + getServiceName() + ":"
+                + getRequestID() + ":" + getErrorCode() + ":"
+                + Util.truncate(getReturnValue(), 64, 64);
+      }
+    };
+  }
 
-	@Override
-	public ClientRequest getResponse() {
-		return this.response;
-	}
-
-	@Override
-	public long getRequestID() {
-		return clientRequestId;
-	}
-
-	@Override
-	public Object getSummary() {
-		return new Object() {
-			@Override
-			public String toString() {
-				return getRequestType() + ":" + getServiceName() + ":"
-						+ getRequestID() + ":" + getErrorCode() + ":"
-						+ Util.truncate(getReturnValue(), 64, 64);
-			}
-		};
-	}
-
-	/**
-	 * Only for instrumentation.
-	 * 
-	 * @param requestID
-	 * @return {@code this}.
-	 */
-	@Deprecated
-	public ClientRequest setClientRequestAndLNSIds(long requestID) {
-		this.clientRequestId = requestID;
-		return this;
-	}
+  /**
+   * Only for instrumentation.
+   *
+   * @param requestID
+   * @return {@code this}.
+   */
+  @Deprecated
+  public ClientRequest setClientRequestAndLNSIds(long requestID) {
+    this.clientRequestId = requestID;
+    return this;
+  }
 }
