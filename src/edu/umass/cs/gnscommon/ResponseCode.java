a9419ab01af85828b80116575449303cee72ca29
Westy <westy@cs.umass.edu>
2016-11-18 13:42:24 -0500

    Cleaned up the documentation in edu.umass.cs.gnscommon.ReponseCode

--- src/edu/umass/cs/gnscommon/ResponseCode.java
+++ src/edu/umass/cs/gnscommon/ResponseCode.java
@@ -26,7 +26,7 @@ import java.util.logging.Level;
 
 /**
  * This class describes the error and exception codes for GNS packets that get
- * sent back to the server and the client.
+ * sent between the server and the clients.
  *
  * These codes are used by the GNS to communicate via response packets why a
  * value could not be returned from the server(s) to the client.
@@ -43,39 +43,31 @@ public enum ResponseCode implements Serializable {
    * For other response codes the return value only contains
    * additional information to describe the error or exception.
    */
-  /**
-   * A positive acknowledgment. This indicates that the
-   * return value contains the result of the command or in
-   * the case of commands that don't return a value that the
-   * command was executed without error.
-   * For other response codes the return value only contains
-   * additional information to describe the error or exception.
-   */
-  NO_ERROR(200, GNSProtocol.OK_RESPONSE.toString(), TYPE.NORMAL),
+  NO_ERROR(200, GNSProtocol.OK_RESPONSE.toString(), ResponseCodeType.NORMAL),
   /**
    * Unspecified error. This should be used replaced with more specific errors
    * in most cases and used sparingly, if at all because it doesn't convey
    * enough information.
    */
-  UNSPECIFIED_ERROR(1, GNSProtocol.UNSPECIFIED_ERROR.toString(), TYPE.ERROR),
+  UNSPECIFIED_ERROR(1, GNSProtocol.UNSPECIFIED_ERROR.toString(), ResponseCodeType.ERROR),
   /**
-   * Field in a record was not found.
+   * A field in a record was not found.
    */
-  FIELD_NOT_FOUND_ERROR(2, GNSProtocol.FIELD_NOT_FOUND.toString(), TYPE.ERROR),
+  FIELD_NOT_FOUND_ERROR(2, GNSProtocol.FIELD_NOT_FOUND.toString(), ResponseCodeType.ERROR),
   // The following three are access or signature errors
   /**
    * Bad signature. A signature error.
    * This will happen when the message in a command packet fails signature verification.
    * See {@link edu.umass.cs.gnsserver.gnsapp.clientSupport.NSAuthentication#signatureAndACLCheck}
    */
-  SIGNATURE_ERROR(5, GNSProtocol.BAD_SIGNATURE.toString(), TYPE.ERROR),
+  SIGNATURE_ERROR(5, GNSProtocol.BAD_SIGNATURE.toString(), ResponseCodeType.ERROR),
   /**
    * Stale signature or key. An access or signature error.
    * This will happen when a command packet arrives that is too old.
    *
    * See {@link edu.umass.cs.gnsserver.gnsapp.clientSupport.NSAuthentication#signatureAndACLCheck}
    */
-  STALE_COMMAND_VALUE(6, GNSProtocol.STALE_COMMMAND.toString(), TYPE.ERROR),
+  STALE_COMMAND_VALUE(6, GNSProtocol.STALE_COMMMAND.toString(), ResponseCodeType.ERROR),
   /**
    * Access denied. An access error.
    * This will happen when a command packet arrives that tries to access a field
@@ -83,52 +75,52 @@ public enum ResponseCode implements Serializable {
    *
    * See {@link edu.umass.cs.gnsserver.gnsapp.clientSupport.NSAuthentication#signatureAndACLCheck}
    */
-  ACCESS_ERROR(7, GNSProtocol.ACCESS_DENIED.toString(), TYPE.ERROR),
+  ACCESS_ERROR(7, GNSProtocol.ACCESS_DENIED.toString(), ResponseCodeType.ERROR),
   /**
    * Non-existent GNSProtocol.GUID.toString().
    * This will happen when a command packet arrives that tries to access a guid
    * that does not exist.
    */
-  BAD_GUID_ERROR(8, GNSProtocol.BAD_GUID.toString(), TYPE.ERROR),
+  BAD_GUID_ERROR(8, GNSProtocol.BAD_GUID.toString(), ResponseCodeType.ERROR),
   /**
    * Non-existent accessor GNSProtocol.GUID.toString().
    * This will happen when a command packet arrives that is trying to access
    * a field using an accessor guid that does not exist.
    */
-  BAD_ACCESSOR_ERROR(9, GNSProtocol.BAD_ACCESSOR_GUID.toString(), TYPE.ERROR),
+  BAD_ACCESSOR_ERROR(9, GNSProtocol.BAD_ACCESSOR_GUID.toString(), ResponseCodeType.ERROR),
   /**
    * An error during account GNSProtocol.GUID.toString() verification.
    * This will happen if the verification code is incorrect.
    */
-  VERIFICATION_ERROR(10, GNSProtocol.VERIFICATION_ERROR.toString(), TYPE.ERROR),
+  VERIFICATION_ERROR(10, GNSProtocol.VERIFICATION_ERROR.toString(), ResponseCodeType.ERROR),
   /**
    * Account guid does not exist.
    *
    */
-  BAD_ACCOUNT_ERROR(11, GNSProtocol.BAD_ACCOUNT.toString(), TYPE.ERROR),
+  BAD_ACCOUNT_ERROR(11, GNSProtocol.BAD_ACCOUNT.toString(), ResponseCodeType.ERROR),
   /**
    * The operation does not exist or does not except the arguments given.
    */
   OPERATION_NOT_SUPPORTED(404, GNSProtocol.OPERATION_NOT_SUPPORTED.toString(),
-          TYPE.ERROR),
+          ResponseCodeType.ERROR),
   /* Errors above, exceptions below. The distinction is that the former is
 	 * more serious and irrecoverable for that operation, but the latter may
 	 * sometimes happen in the otherwise normal course of events. */
   /**
    * Account has already been verified.
    */
-  ALREADY_VERIFIED_EXCEPTION(12, GNSProtocol.ALREADY_VERIFIED_EXCEPTION.toString(), TYPE.EXCEPTION),
+  ALREADY_VERIFIED_EXCEPTION(12, GNSProtocol.ALREADY_VERIFIED_EXCEPTION.toString(), ResponseCodeType.EXCEPTION),
   /**
    * Duplicate GNSProtocol.GUID.toString() or HRN.
    */
   DUPLICATE_ID_EXCEPTION(14,
           ClientReconfigurationPacket.ResponseCodes.DUPLICATE_ERROR
-          .toString(), TYPE.EXCEPTION),
+          .toString(), ResponseCodeType.EXCEPTION),
   /**
    * Duplicate field in a record.
    */
   DUPLICATE_FIELD_EXCEPTION(15, GNSProtocol.DUPLICATE_FIELD.toString(),
-          TYPE.EXCEPTION),
+          ResponseCodeType.EXCEPTION),
   /**
    * The name for which a RequestActiveReplicas was sent was found to not
    * exist at a reconfigurator. This can happen simply because a laggard
@@ -146,7 +138,7 @@ public enum ResponseCode implements Serializable {
    */
   NONEXISTENT_NAME_EXCEPTION(16,
           ClientReconfigurationPacket.ResponseCodes.NONEXISTENT_NAME_ERROR
-          .toString(), TYPE.EXCEPTION),
+          .toString(), ResponseCodeType.EXCEPTION),
   /**
    * When an application request is received by an active replica where no
    * replica for the name exists. This does not necessarily mean that the name
@@ -157,65 +149,65 @@ public enum ResponseCode implements Serializable {
    */
   ACTIVE_REPLICA_EXCEPTION(17,
           ClientReconfigurationPacket.ResponseCodes.ACTIVE_REPLICA_EXCEPTION
-          .toString(), TYPE.EXCEPTION),
+          .toString(), ResponseCodeType.EXCEPTION),
   /**
    * The alias does not exist.
    */
-  BAD_ALIAS_EXCEPTION(18, GNSProtocol.BAD_ALIAS.toString(), TYPE.EXCEPTION),
+  BAD_ALIAS_EXCEPTION(18, GNSProtocol.BAD_ALIAS.toString(), ResponseCodeType.EXCEPTION),
   /**
    * The ACL type does not exist.
    */
-  BAD_ACL_TYPE_ERROR(19, GNSProtocol.BAD_ACL_TYPE.toString(), TYPE.ERROR),
+  BAD_ACL_TYPE_ERROR(19, GNSProtocol.BAD_ACL_TYPE.toString(), ResponseCodeType.ERROR),
   /**
    * The field does not exist.
    */
-  FIELD_NOT_FOUND_EXCEPTION(20, GNSProtocol.FIELD_NOT_FOUND.toString(), TYPE.EXCEPTION),
+  FIELD_NOT_FOUND_EXCEPTION(20, GNSProtocol.FIELD_NOT_FOUND.toString(), ResponseCodeType.EXCEPTION),
   /**
    * The guid is a duplicate of an already existing guid.
    */
-  DUPLICATE_GUID_EXCEPTION(21, GNSProtocol.DUPLICATE_GUID.toString(), TYPE.EXCEPTION),
+  DUPLICATE_GUID_EXCEPTION(21, GNSProtocol.DUPLICATE_GUID.toString(), ResponseCodeType.EXCEPTION),
   /**
    * The HRN already exists.
    */
-  DUPLICATE_NAME_EXCEPTION(22, GNSProtocol.DUPLICATE_NAME.toString(), TYPE.EXCEPTION),
+  DUPLICATE_NAME_EXCEPTION(22, GNSProtocol.DUPLICATE_NAME.toString(), ResponseCodeType.EXCEPTION),
   /**
    * A JSON parsing error occurred.
    */
-  JSON_PARSE_ERROR(23, GNSProtocol.JSON_PARSE_ERROR.toString(), TYPE.ERROR),
+  JSON_PARSE_ERROR(23, GNSProtocol.JSON_PARSE_ERROR.toString(), ResponseCodeType.ERROR),
   /**
    * The max alias limit was exceeded.
    */
-  TOO_MANY_ALIASES_EXCEPTION(24, GNSProtocol.TOO_MANY_ALIASES.toString(), TYPE.EXCEPTION),
+  TOO_MANY_ALIASES_EXCEPTION(24, GNSProtocol.TOO_MANY_ALIASES.toString(), ResponseCodeType.EXCEPTION),
   /**
    * The max guid limit was exceeded.
    */
-  TOO_MANY_GUIDS_EXCEPTION(25, GNSProtocol.TOO_MANY_GUIDS.toString(), TYPE.EXCEPTION),
+  TOO_MANY_GUIDS_EXCEPTION(25, GNSProtocol.TOO_MANY_GUIDS.toString(), ResponseCodeType.EXCEPTION),
   /**
    * There was an error while updating a record.
    */
-  UPDATE_ERROR(26, GNSProtocol.UPDATE_ERROR.toString(), TYPE.ERROR),
+  UPDATE_ERROR(26, GNSProtocol.UPDATE_ERROR.toString(), ResponseCodeType.ERROR),
   /**
    * Something went wrong while we were reading from or writing to the database.
    */
-  DATABASE_OPERATION_ERROR(27, GNSProtocol.DATABASE_OPERATION_ERROR.toString(), TYPE.ERROR),
+  DATABASE_OPERATION_ERROR(27, GNSProtocol.DATABASE_OPERATION_ERROR.toString(), ResponseCodeType.ERROR),
   /**
    * An error occurred during the processing of a command query.
    */
-  QUERY_PROCESSING_ERROR(405, GNSProtocol.QUERY_PROCESSING_ERROR.toString(), TYPE.ERROR),
+  QUERY_PROCESSING_ERROR(405, GNSProtocol.QUERY_PROCESSING_ERROR.toString(), ResponseCodeType.ERROR),
   /**
    * A timeout occurred.
    */
-  TIMEOUT(408, GNSProtocol.TIMEOUT.toString(), TYPE.EXCEPTION),
+  TIMEOUT(408, GNSProtocol.TIMEOUT.toString(), ResponseCodeType.EXCEPTION),
   /**
    * A remote query failed on the server side.
    */
-  REMOTE_QUERY_EXCEPTION(410, GNSProtocol.REMOTE_QUERY_EXCEPTION.toString(), TYPE.EXCEPTION),
+  REMOTE_QUERY_EXCEPTION(410, GNSProtocol.REMOTE_QUERY_EXCEPTION.toString(), ResponseCodeType.EXCEPTION),
   /**
    * An internal request, possibly an active request, failed probably because the TTL expired
    * or it was attempting to cause a cycle.
    */
   INTERNAL_REQUEST_EXCEPTION(411, GNSProtocol.INTERNAL_REQUEST_EXCEPTION
-          .toString(), TYPE.EXCEPTION),;
+          .toString(), ResponseCodeType.EXCEPTION),;
 
   // stash the codes in a lookup table
   private static final Map<Integer, ResponseCode> responseCodes = new HashMap<>();
@@ -245,13 +237,13 @@ public enum ResponseCode implements Serializable {
   //
   private final int codeValue;
   private final String protocolCode;
-  private final TYPE type;
+  private final ResponseCodeType type;
   private String message = null;
 
   /**
    * The response code category.
    */
-  public static enum TYPE {
+  public static enum ResponseCodeType {
     /**
      * No error or exceptional condition.
      */
@@ -266,7 +258,7 @@ public enum ResponseCode implements Serializable {
     ERROR;
   };
 
-  private ResponseCode(int codeValue, String protocolCode, TYPE type) {
+  private ResponseCode(int codeValue, String protocolCode, ResponseCodeType type) {
     this.codeValue = codeValue;
     this.protocolCode = protocolCode;
     this.type = type;
@@ -316,7 +308,7 @@ public enum ResponseCode implements Serializable {
    * @return true if this is an error
    */
   public boolean isExceptionOrError() {
-    return type == TYPE.ERROR || type == TYPE.EXCEPTION;
+    return type == ResponseCodeType.ERROR || type == ResponseCodeType.EXCEPTION;
   }
 
   /**
@@ -335,14 +327,14 @@ public enum ResponseCode implements Serializable {
    * @return true if this is an error
    */
   public boolean isError() {
-    return type == TYPE.ERROR;
+    return type == ResponseCodeType.ERROR;
   }
 
   /**
    * @return True if this is an exception.
    */
   public boolean isException() {
-    return type == TYPE.EXCEPTION;
+    return type == ResponseCodeType.EXCEPTION;
   }
 
   private static String generateSwiftConstants() {
